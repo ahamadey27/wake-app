92aec2245f9f61ba5c6773764ca37db3623d94f2
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-06-05 14:35:17 -0400

    small bug fix

--- WakeAdvisor/obj/Debug/net9.0/WakeAdvisor.AssemblyInfo.cs
+++ WakeAdvisor/obj/Debug/net9.0/WakeAdvisor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WakeAdvisor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf35458983ba93beafd971d523b6089fbf46ac31")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a06f4efe07e7eb2a9c7b745e83e3f62065bbee7")]
 [assembly: System.Reflection.AssemblyProductAttribute("WakeAdvisor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WakeAdvisor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
